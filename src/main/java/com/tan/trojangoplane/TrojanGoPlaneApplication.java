fe221988559f2696a7a4dfe298b969a159a798a0
1660233887@qq.com <1660233887*txb>
2020-10-31 20:25:01 +0800

    初始化

---
+++ src/main/java/com/tan/trojangoplane/TrojanGoPlaneApplication.java
@@ -0,0 +1,13 @@
+package com.tan.trojangoplane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrojanGoPlaneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrojanGoPlaneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tan/trojangoplane/TrojanGoPlaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tan.trojangoplane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrojanGoPlaneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
